530e2caea5f5f1eb62da319a2f0bd29d6c1c1960
Robert Dunbar <robert_dunbar@me.com>
2019-07-20 09:59:02 +0100

    better version of fibs

--- fib.rb
+++ fib.rb
@@ -2,13 +2,12 @@ def fibs(num)
     return [0] if num == 0
     return [0,1] if num == 1
     array = [0,1]
-    2.upto(num - 1) { |n| array[n] = array[n - 2] + array[n-1] }
+    2.upto(num) { |n| array[n] = array[-2] + array[-1] }
     return array
 end
 
 def fibs_rec(num, arr=[0,1])
-    return arr if num < 3
-    return fibs_rec(num - 1, arr << arr[-2] + arr [-1])
+    return num == 0 ? [0] : num == 1? arr : fibs_rec(num - 1, arr << arr[-2] + arr [-1])
 end
 
 p fibs (10)
